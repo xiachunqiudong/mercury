4a624af57f1085ffea131604d36e9cd05ad74c6a
xiadong <xiadaoliang@outlook.com>
2025-08-07 08:20:56 -0700

    update rtl/pkg/mercury_pkg.sv

--- rtl/pkg/mercury_pkg.sv
+++ rtl/pkg/mercury_pkg.sv
@@ -1,19 +1,19 @@
 package mercury_pkg;
 
-  typedef enum {
-    NONE,
-    LUI,
-    AUIPC,
-    JAL, JALR,
-    BEQ, BNE, BLT, BGE, BLTU, BGEU,
-    LB, LH, LW, LBU, LHU, LWU, LD,
-    SB, SH, SW, SD,
-    ADDI, ADDIW, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SLLIW, SRLI, SRLIW, SRAI, SRAIW,
-    ADD, ADDW, SUB, SUBW, SLL, SLLW, SLT, SLTU, XOR, SRL, SRLW, SRA, SRAW, OR, AND,
-    FENCE, FENCE_TSO,
-    PAUSE,
-    ENV
-  } opcode1_t;
+  //typedef enum {
+  //  NONE,
+  //  LUI,
+  //  AUIPC,
+  //  JAL, JALR,
+  //  BEQ, BNE, BLT, BGE, BLTU, BGEU,
+  //  LB, LH, LW, LBU, LHU, LWU, LD,
+  //  SB, SH, SW, SD,
+  //  ADDI, ADDIW, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SLLIW, SRLI, SRLIW, SRAI, SRAIW,
+  //  ADD, ADDW, SUB, SUBW, SLL, SLLW, SLT, SLTU, XOR, SRL, SRLW, SRA, SRAW, OR, AND,
+  //  FENCE, FENCE_TSO,
+  //  PAUSE,
+  //  ENV
+  //} opcode1_t;
 
   typedef enum {
     NONE,
@@ -21,8 +21,9 @@ package mercury_pkg;
     AUIPC,
     JAL, JALR, BRANCH,
     LOAD, STORE,
+    ALU, ALUI,
     FENCE,
-    ECALL, EBREAK
+    ENV
   } opcode_t;
 
   typedef enum {
